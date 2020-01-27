34ce7cc212b1c6506a4cefaa42cb72dd3d838d23
lucasluz99 <lucasluzthatsme@gmail.com>
2020-01-27 00:42:46 -0300

    method store of appointmentcontroller

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,43 @@
+import * as Yup from 'yup';
+
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
+
+    const { date, provider_id } = req.body;
+
+    const isProvider = await User.findOne({
+      where: {
+        id: provider_id,
+        provider: true,
+      },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'You can only create appointments with providers' });
+    }
+
+    const appointment = await Appointment.create({
+      date,
+      provider_id,
+      user_id: req.userId,
+      cancelad_at: null,
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -11,6 +11,7 @@ class Appointments extends Model {
         sequelize,
       }
     );
+    return this;
   }
 
   static associate(models) {
