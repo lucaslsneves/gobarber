496547c091c60ae1e3286e664ac87d557d208b5f
lucasluz99 <lucasluzthatsme@gmail.com>
2020-01-26 23:26:22 -0300

    Add avatar

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-
+import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -17,7 +17,5 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
