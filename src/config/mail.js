5d7eeed78891a0a20ca00eb2188aabf0031be84c
lucasluz99 <lucasluzthatsme@gmail.com>
2020-01-27 03:19:54 -0300

    add mail config

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '658b04200f59c6',
+    pass: 'a8762478018194',
+  },
+  default: {
+    from: 'Lucas Luz <noreply@lucasluz.com>',
+  },
+};
